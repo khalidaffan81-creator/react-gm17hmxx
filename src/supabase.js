8ca281f4f4dd18be6aea5c09891a5d80473b36bc
khalidaffan81-creator <khalidaffan81@gmail.com>
2026-02-28 23:44:13 +0530

    some minor bug fixing

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = https://rhjxttrfrunbytmhrjzp.supabase.co "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = sb_publishable_U8f8NnWUfdWtGegkWBBMlg_8P-CP4uY "YOUR_SUPABASE_ANON_KEY";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(
+    "https://rhjxttrfrunbytmhrjzp.supabase.co",
+    "sb_publishable_U8f8NnWUfdWtGegkWBBMlg_8P-CP4uY"
+);
